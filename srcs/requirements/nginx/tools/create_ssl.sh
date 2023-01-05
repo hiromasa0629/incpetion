4bc55f72531699c6db1e399e3b04b1637cdade8d
hiro <howzheng0629@gmail.com>
2023-01-05 16:04:27 +0800

    .

--- srcs/requirements/nginx/tools/create_ssl.sh
+++ srcs/requirements/nginx/tools/create_ssl.sh
@@ -2,6 +2,7 @@
 
 CERTS_PATH="/etc/nginx/certs"
 
+mkdir -p $CERTS_PATH
 openssl genrsa -out $CERTS_PATH/private.key 2048
 openssl req -new -key $CERTS_PATH/private.key -out $CERTS_PATH/csr.csr -subj "/C=MY/ST=KL/L=KL/O=42/OU=42KL/CN=Hiro"
 openssl x509 -req -days 365 -in $CERTS_PATH/csr.csr -signkey $CERTS_PATH/private.key -out $CERTS_PATH/certificate.crt
