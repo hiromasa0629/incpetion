1a5ce09acc968e15078d9f93a66409ad9d6356dd
hiromasa0629 <howzheng0629@gmail.com>
2023-01-11 20:38:17 +0800

    final

--- Makefile
+++ Makefile
@@ -1,23 +1,25 @@
 COMPOSE_YML=./srcs/docker-compose.yml
-WORDPRESS_SRCS=./srcs/requirements/wordpress/tools/srcs
-MARIADB_SRCS=./srcs/requirements/mariadb/tools/srcs
+WORDPRESS_SRCS=/home/hyap/data/wordpress
+MARIADB_SRCS=/home/hyap/data/wordpressdb
 
 all:
-	mkdir -p $(WORDPRESS_SRCS)
-	mkdir -p $(MARIADB_SRCS)
+	sudo mkdir -p ${WORDPRESS_SRCS}
+	sudo mkdir -p ${MARIADB_SRCS}
 	sudo ROOT_PWD=${PWD} docker compose -f $(COMPOSE_YML) --verbose up -d
 down:
 	sudo ROOT_PWD=${PWD} docker compose -f $(COMPOSE_YML) down
 
 clean: down
-	sudo docker network prune -f
-	sudo docker volume prune -f
 	sudo docker image prune -a -f
+	sudo docker volume prune -f
+	sudo docker volume rm `sudo docker volume ls -q`
+	sudo docker network prune -f
 
 fclean: clean
 	sudo rm -rf $(WORDPRESS_SRCS)
+	sudo rm -rf ${MARIADB_SRCS}
 	sudo docker system prune -a -f
 
-re: clean all
+re: fclean all
 
 .PHONY: all down clean fclean re
--- srcs/requirements/mariadb/tools/configure_db.sh
+++ srcs/requirements/mariadb/tools/configure_db.sh
@@ -8,11 +8,13 @@ mysql -u root -p"$DATABASE_ROOT_PASS" -e "SET PASSWORD FOR 'root'@localhost = PA
 mysql -u root -p"$DATABASE_ROOT_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')" && \
 mysql -u root -p"$DATABASE_ROOT_PASS" -e "DELETE FROM mysql.user WHERE User=''" && \
 mysql -u root -p"$DATABASE_ROOT_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'" && \
-mysql -u root -p"$DATABASE_ROOT_PASS" -e "CREATE DATABASE $DATABASE_NAME" && \
 mysql -u root -p"$DATABASE_ROOT_PASS" -e "CREATE USER '$DATABASE_USER'@'%' IDENTIFIED BY '$DATABASE_USER_PASS'" && \
 mysql -u root -p"$DATABASE_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USER'@'%'" && \
-mysql -u root -p"$DATABASE_ROOT_PASS" -e "FLUSH PRIVILEGES" && \
-mysql -u root -p"$DATABASE_ROOT_PASS" "$DATABASE_NAME" < ./wordpressdb.sql
+mysql -u root -p"$DATABASE_ROOT_PASS" -e "FLUSH PRIVILEGES"
+
+chown -R mysql:mysql "/var/lib/mysql/$DATABASE_NAME"
+
+mysql -u root -p"$DATABASE_ROOT_PASS" "$DATABASE_NAME" < "./$DATABASE_NAME.sql"
 
 sed -i "5s/.*/password = $DATABASE_ROOT_PASS/;10s/.*/password = $DATABASE_ROOT_PASS/" /etc/mysql/debian.cnf
 service mysql stop
